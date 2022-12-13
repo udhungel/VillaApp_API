dad531e36ed4440baf1d13042a24879d41cd4351
udhungel <udeepdhungel@gmail.com>
2022-12-13 20:50:36 +0100

    First End Point - Add VillaDTO

--- Controllers/VillaAPIController.cs
+++ Controllers/VillaAPIController.cs
@@ -1,20 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using VillaApp_WebAPI.Models;
+using VillaApp_WebAPI.Models.Dto;
 
 namespace VillaApp_WebAPI.Controllers
 {
-
+    //[Route("api/[controller]")] // not the best approach 
     [Route("api/VillaAPI")] // does not have an attribute route exception 
     [ApiController]
     public class VillaAPIController : ControllerBase
     {
 
         [HttpGet] //failed to lod API defination. Endpoint we add needs to be defined HTTP get or POST
-        public IEnumerable<Villa> GetVillas()
+        public IEnumerable<VillaDTO> GetVillas()
         {
-            return new List<Villa>() { new Villa() { Id = 1, Name = "Pool View " },
-                                       new Villa() { Id = 2, Name = "Beach View " }
-                                     };
+            return new List<VillaDTO>() { new VillaDTO() { Id = 1, Name = "Pool View " },
+                                          new VillaDTO() { Id = 2, Name = "Beach View " }
+                                        };
         }   
 
 
