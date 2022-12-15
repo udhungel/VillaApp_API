7528b8d2d01427dcd59033f6e169800d1d335818
udhungel <udeepdhungel@gmail.com>
2022-12-15 21:14:09 +0100

    Dependency Injection - Adding Nuget Pakages and updating Model -Nuget Packages Microsoft.EntityFrameworkCore.Tools Microsoft.EntityFrameWorkCore.SqlServer dependent on Microsoft.EntityFrameworkCore. Relational

--- Models/Villa.cs
+++ Models/Villa.cs
@@ -3,7 +3,19 @@
     public class Villa
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Name { get; set; }     
+
+        public double Rate { get; set; }
+        public string Details { get; set; }
+        public int Sqft { get; set; }
+        public int Occupany { get; set; }
+        public string ImageUrl { get; set; }
+        public string Amenity { get; set; }
         public DateTime CreatedDate { get; set; }
+        public DateTime UpdateDate { get; set; }
+
+
+
+
     }
 }
