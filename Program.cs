93520566e1148e497b9febcd4fa327c27f4e4a99
udhungel <udeepdhungel@gmail.com>
2022-12-15 19:40:02 +0100

    Dependency Inject - Serilog Program.cs file changes

--- Program.cs
+++ Program.cs
@@ -1,8 +1,14 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+//logger configuration using SiriLog
+Log.Logger = new LoggerConfiguration().MinimumLevel.Debug().WriteTo.File("log/villaLogs.txt",rollingInterval:RollingInterval.Day).CreateLogger();
+
 builder.Services.AddControllers(options =>{
-                                             //options.ReturnHttpNotAcceptable = true; //text/plain need to comment it
+                                             //options.ReturnHttpNotAcceptable = true; //for text/plain need to comment it
                                           }).AddNewtonsoftJson().AddXmlDataContractSerializerFormatters();
                                             
 builder.Services.AddControllers();
