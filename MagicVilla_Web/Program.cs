65b2e63afe70fdbc52ec78510ce5e4cf4c7b185f
udhungel <udeepdhungel@gmail.com>
2023-12-26 22:09:05 +0100

    Consuming API : Dto for Automapper together with nuget package and service configuration

--- MagicVilla_Web/Program.cs
+++ MagicVilla_Web/Program.cs
@@ -1,7 +1,12 @@
+using MagicVilla_Web.AutoMapper;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//AutoMapper
+builder.Services.AddAutoMapper(typeof(MappingConfig));
 
 var app = builder.Build();
 
