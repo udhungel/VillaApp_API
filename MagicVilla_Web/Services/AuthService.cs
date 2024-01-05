7cc9b8b6fa2bf5845d6b205bdd59400e8297f8a7
udhungel <udeepdhungel@gmail.com>
2024-01-05 22:43:58 +0100

    Consuming Secured API - Adding AuthService

---
+++ MagicVilla_Web/Services/AuthService.cs
@@ -0,0 +1,41 @@
+﻿using MagicVilla_Utility;
+using MagicVilla_Web.Models.Dto;
+
+namespace MagicVilla_Web.Services
+{
+    public class AuthService : BaseService, IAuthService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private string villaUrl;
+
+        public AuthService(IHttpClientFactory httpClientFactory, IConfiguration configuration) : base(httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+            villaUrl = configuration.GetValue<string>("ServiceUrls:VillaAPI");
+
+        }
+
+        public Task<T> LoginAsync<T>(LoginResponsetDto obj)
+        {
+            return SendAsync<T>(new APIRequest()
+            {
+                ApiType = SD.ApiType.POST,
+                Data = obj,
+                Url = villaUrl + "api/UsersAuth/login"
+
+            });
+        }
+
+        public Task<T> RegisterAsync<T>(RegistrationRequestDto obj)
+        {
+            return SendAsync<T>(new APIRequest()
+            {
+                ApiType = SD.ApiType.POST,
+                Data = obj,
+                Url = villaUrl + "api/UsersAuth/register"
+
+            });
+        }
+    }
+}
+
---
+++ MagicVilla_Web/Services/IAuthService.cs
@@ -0,0 +1,12 @@
+﻿using MagicVilla_Web.Models.Dto;
+
+namespace MagicVilla_Web.Services
+{
+    public interface IAuthService
+    {
+        Task<T> LoginAsync<T>(LoginResponsetDto objToCreate);
+
+        Task<T> RegisterAsync<T>(RegistrationRequestDto registrationRequestDto);
+
+    }
+}
