30380e5afb561fb41006b8b6f47826b82accf9b8
udhungel <udeepdhungel@gmail.com>
2024-01-05 23:00:24 +0100

    Consuming Secured API : Login and Register Action Method

---
+++ MagicVilla_Web/Controllers/AuthController.cs
@@ -0,0 +1,67 @@
+﻿using MagicVilla_Web.Models;
+using MagicVilla_Web.Models.Dto;
+using MagicVilla_Web.Services;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace MagicVilla_Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            LoginResponsetDto obj = new LoginResponsetDto();
+            return View(obj);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginRequestDto loginRequestDto)
+        {           
+            return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegistrationRequestDto registrationRequestDto)
+        {
+            APIResponse apiResponse = await _authService.RegisterAsync<APIResponse>(registrationRequestDto);
+            if (apiResponse !=null && apiResponse.IsSucess)
+            {
+                return RedirectToAction("Login");
+
+            }
+            return View();
+        }
+        
+        [HttpGet]
+        public async Task<IActionResult> Logout()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+
+
+    }
+}
--- MagicVilla_Web/Models/Dto/LoginResponseDto.cs
+++ MagicVilla_Web/Models/Dto/LoginResponseDto.cs
@@ -1,7 +1,9 @@
-﻿{
+﻿namespace MagicVilla_Web.Models.Dto  
+{
     public class LoginResponsetDto
     {
         public UserDto User { get; set; }
         public string Token { get; set; }
     }
 }
+
