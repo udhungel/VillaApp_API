2601bea1eec9ea19d15a0fb17f3b9239897b0c88
udhungel <udeepdhungel@gmail.com>
2024-01-05 22:19:36 +0100

    Consuming Secured API - Adding Dtos for Login andRegistrations

---
+++ MagicVilla_Web/Models/Dto/LoginResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace MagicVilla_Web.Models.Dto
+{
+    public class LoginResponsetDto
+    {
+        public UserDto User { get; set; }
+        public string Token { get; set; }
+    }
+}
---
+++ MagicVilla_Web/Models/Dto/UserDto.cs
@@ -0,0 +1,11 @@
+﻿namespace MagicVilla_Web.Models.Dto
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; }
+        public string Role { get; set; }
+        public string Name { get; set; }
+    }
+}
