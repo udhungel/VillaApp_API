28ac4eba3c86179c341d8b6f8b31837ad46dd627
udhungel <udeepdhungel@gmail.com>
2024-01-09 00:01:13 +0100

    BugFix : Need to set the JWT token name as unique_name from .NET 7 and above Added missing Session object for the token from last commit

--- MagicVilla_Web/Models/Dto/UserDto.cs
+++ MagicVilla_Web/Models/Dto/UserDto.cs
@@ -2,10 +2,9 @@
 {
     public class UserDto
     {
-        public int Id { get; set; }
+        public string  Id { get; set; }
         public string Username { get; set; } = string.Empty;
-        public string Password { get; set; }
-        public string Role { get; set; }
+        public string Password { get; set; }      
         public string Name { get; set; }
     }
 }
