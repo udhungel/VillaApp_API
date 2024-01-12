e4ad3a3c795698f619ff0c5c825bec6b00c2119f
udhungel <udeepdhungel@gmail.com>
2024-01-12 22:47:15 +0100

    update image url - 1

---
+++ Villa_WebAPI/Migrations/20240112214527_UpdateImageURL01.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VillaAppWebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateImageURL01 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ImageUrl",
+                value: "https://placehold.co/600x400");
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ImageUrl",
+                value: "https://placehold.co/600x401");
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ImageUrl",
+                value: "https://placehold.co/600x402");
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ImageUrl",
+                value: "https://placehold.co/600x403");
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ImageUrl",
+                value: "https://placehold.co/600x404");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ImageUrl",
+                value: "https://placehold.co/600x400");
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ImageUrl",
+                value: "https://placehold.co/600x401");
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ImageUrl",
+                value: "https://placehold.co/600x402");
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ImageUrl",
+                value: "https://placehold.co/600x403");
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ImageUrl",
+                value: "https://placehold.co/600x404");
+        }
+    }
+}
